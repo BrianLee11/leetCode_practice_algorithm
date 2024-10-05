8a41009e7c73413b4c2f37b5b96a536b5804ab21
Brian J. Lee <134612285+BrianLee11@users.noreply.github.com>
2024-10-05 12:38:23 -0400

    Update 167_two_sum_II_input_array_is_sorted.py

--- weeklyLeetCode/week1_list_array_sort/batch2/167_two_sum_II_input_array_is_sorted.py
+++ weeklyLeetCode/week1_list_array_sort/batch2/167_two_sum_II_input_array_is_sorted.py
@@ -29,12 +29,13 @@ class Solution(object):
         # visit number in the array numbers once
         # The total time complexity of the for loop is O(n) because inside the for loop, the time complexity is O(1)
         # The total space complexity of the for loop is O(n) because inside the for loop, the space complexity is O(1)
+        
         for index in range(len(numbers)):
-            reciprocal = target - numbers[index]  # time complexity: O(1), space complexity: O(1)
+            reciprocal = target - numbers[index]                 # time complexity: O(1), space complexity: O(1)
 
             # Check if the reciprocal exists in the dictionary
-            if reciprocal in dictionary:          # time compleixty: O(1) as searching in hash table. Space complexity: O(1)
+            if reciprocal in dictionary:                         # time compleixty: O(1) as searching in hash table. Space complexity: O(1)
                 return [dictionary[reciprocal] + 1, index + 1]   # time complexity: O(1) as simple operation. Space compleixty: O(1)
             else:
                  # Store the current number with its index
-                dictionary[numbers[index]] = index  # time complexity: O(1) as adding a (key,value)in hash table is constant operation. Space complexity: O(1)
+                dictionary[numbers[index]] = index               # time complexity: O(1) as adding a (key,value)in hash table is constant operation. Space complexity: O(1)
